d0a5937cd153aa0bd0f93791886160826d32cf98
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-04-18 17:59:48 +0300

    manually added rollup

--- apps/react-webpack/src/app/app.stories.tsx
+++ apps/react-webpack/src/app/app.stories.tsx
@@ -1,18 +1,12 @@
 import type { Meta } from '@storybook/react';
-import {  App  }  from './app';
-
-
-
-
+import { App } from './app';
 
 const Story: Meta<typeof App> = {
   component: App,
-  title: 'App', 
+  title: 'App',
 };
 export default Story;
 
-
 export const Primary = {
-  args: {
-  },
+  args: {},
 };
