08ca0a0f9b5c56a3a2decee89654dd8f4658b8cb
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-04-18 17:52:12 +0300

    storybook configuration

---
+++ apps/next-app/components/one/one.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta } from '@storybook/react';
+import {  One  }  from './one';
+
+
+
+
+
+const Story: Meta<typeof One> = {
+  component: One,
+  title: 'One', 
+};
+export default Story;
+
+
+export const Primary = {
+  args: {
+  },
+};
