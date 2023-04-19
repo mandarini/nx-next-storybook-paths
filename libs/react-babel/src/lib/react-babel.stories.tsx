a3284c8dc03060b5b6d50b142e06cd31deef3fe3
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-04-19 17:21:06 +0300

    sb for react babel

---
+++ libs/react-babel/src/lib/react-babel.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta } from '@storybook/react';
+import {  ReactBabel  }  from './react-babel';
+
+
+
+
+
+const Story: Meta<typeof ReactBabel> = {
+  component: ReactBabel,
+  title: 'ReactBabel', 
+};
+export default Story;
+
+
+export const Primary = {
+  args: {
+  },
+};
